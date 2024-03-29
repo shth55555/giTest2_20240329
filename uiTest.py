4a707691e21e0e42ae8dbdb8dbe515c3af4b3bce
shth55555 <shth5555@naver.com>
2024-03-29 14:20:36 +0900

    ui 테스트 호출

--- uiTest.py
+++ uiTest.py
@@ -14,6 +14,16 @@ class MyWindow(QMainWindow, form_class):
         super().__init__()   #부모 클래스 생성자 호출
         self.setupUi(self)    #위에서 불러온 test.ui를 연결
         self.setWindowTitle("연습 프로그램")
+        self.button1.clicked.connect(self.button1_click)
+        self.button2.clicked.connect(self.button2_click)
+
+    def button1_click(self):
+        self.label1.setText("Hellow world!!!")
+
+    def button2_click(self):
+        self.label1.setText("안녕하세요!")
+
+
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
